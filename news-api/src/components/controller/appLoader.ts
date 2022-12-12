74b7ecd6ddd2555d15058164253e09bb36e4ba3f
ULADZISLAVA <vladislava96.96@mail.ru>
2022-12-12 22:30:41 +0300

    fix: change API url

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://newsapi-redirect-production.up.railway.app/', {
             apiKey: '8ea513fce93b43a9be65b1dadce6fb0f', // получите свой ключ https://newsapi.org/
         });
     }
